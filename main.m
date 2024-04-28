921a0dde0e893e0b3baf50b470a10379853b541a
pengubow <66965109+pengubow@users.noreply.github.com>
2024-04-28 22:31:49 +0300

    Update main.m

--- main.m
+++ main.m
@@ -5,27 +5,66 @@
 extern char** environ;
 int ptrace(int, pid_t, caddr_t, int);
 
+#define CS_DEBUGGED 0x10000000
+int csops(pid_t pid, unsigned int ops, void *useraddr, size_t usersize);
+
+//this is copied from https://stackoverflow.com/questions/6530364/how-to-detect-that-the-app-is-running-on-a-jailbroken-device
+
+bool fileExistsAtPath(NSString* path) {
+    FILE *pFile;
+    pFile = fopen([path cStringUsingEncoding:[NSString defaultCStringEncoding]], "r");
+    if (pFile == NULL) {
+        return false;
+    }
+    else
+        fclose(pFile);
+    return true;
+}
+//very simple paths
+bool isJailBroken() {
+
+    NSArray *paths = @[@"/var/jb/Applications/Sileo.app",
+                       @"/var/jb/Applications/Zebra.app",
+                       @"/Applications/Cydia.app",
+                       ];
+
+    for (NSString *path in paths) {
+        if (fileExistsAtPath(path)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
+//getting if it has jit. its copied from what do you thing where?
+
+bool Pojavlaunchercheckforjit(int pid) {
+    
+
+    int flags;
+    csops(pid, 0, &flags, sizeof(flags));
+    bool Doesithavejit = (flags & CS_DEBUGGED) != 0;
+    if (!Doesithavejit) {
+        sleep(1);
+        Pojavlaunchercheckforjit(pid);
+    }
+    return true; //thank you for jit
+}
+
+
 __attribute__((constructor)) static void entry(int argc, char **argv, char *envp[])
 {
+    int pid = [[NSProcessInfo processInfo] processIdentifier];
     NSLog(@"mrow INIT");
-    if (argc > 1 && strcmp(argv[1], "--jit") == 0) {
-	    NSLog(@"mrow exiting");
-            ptrace(0, 0, 0, 0);
-            exit(0);
-        } else {
-	    NSLog(@"mrow crac");
-            pid_t pid;
-            char *modified_argv[] = {argv[0], "--jit", NULL };
-            int ret = posix_spawnp(&pid, argv[0], NULL, NULL, modified_argv, envp);
-            if (ret == 0) {
-                waitpid(pid, NULL, WUNTRACED);
-                ptrace(11, pid, 0, 0);
-                kill(pid, SIGTERM);
-                wait(NULL);
-            }
-        }
+    if (!isJailBroken()) {Pojavlaunchercheckforjit(pid);}
+   
+            
+
     init_bypassDyldLibValidation();
     init_fixCydiaSubstrate();
 
     init_loadGeode();
+    
 }
